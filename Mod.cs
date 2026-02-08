272c319ec1d844c423c5adf20ed8f730ee620d7a
Nick Morrison <nickmorrison09@gmail.com>
2026-02-07 20:38:35 -0500

    Added system to mod

--- Mod.cs
+++ Mod.cs
@@ -2,6 +2,8 @@
 using Game;
 using Game.Modding;
 using Game.SceneFlow;
+using Game.Simulation;
+using TransitStats.Systems;
 
 namespace TransitStats
 {
@@ -12,10 +14,11 @@ namespace TransitStats
         public void OnLoad(UpdateSystem updateSystem)
         {
             log.Info(nameof(OnLoad));
-
             if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
                 log.Info($"Current mod asset at {asset.path}");
 
+            updateSystem.UpdateAfter<CitizenBehaviorSystem, CitizenTransitTransferSystem>(SystemUpdatePhase.GameSimulation);
+            updateSystem.UpdateBefore<CityStatisticsSystem, CitizenTransitTransferSystem>(SystemUpdatePhase.GameSimulation);
         }
 
         public void OnDispose()
